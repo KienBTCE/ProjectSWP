e742280f8f2a52dd79e0fbbf9671add3ca8858c1
MrNhut123 <nhutbuist949@gmail.com>
2024-12-04 16:53:17 +0700

    Create new database function

--- FShopSQLQuery.sql
+++ FShopSQLQuery.sql
@@ -1,3 +1,20 @@
-CREATE DATABASE FSHOP
+USE [master]
+GO
 
-USE FSHOP
+/*******************************************************************************
+   Drop database if it exists
+********************************************************************************/
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'FShop')
+BEGIN
+	ALTER DATABASE FShop SET OFFLINE WITH ROLLBACK IMMEDIATE;
+	ALTER DATABASE FShop SET ONLINE;
+	DROP DATABASE FShop;
+END
+
+GO
+
+CREATE DATABASE FShop
+GO
+
+USE FShop
+GO
