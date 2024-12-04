9b85ffb06b8cd13910c33c30e2749b6ec6e4726a
KienBTCE <KienBTCE180180@fpt.edu.vn>
2024-12-04 16:42:27 +0700

    create string connection in DBContext.java

--- FShop/src/main/java/DB/DBContext.java
+++ FShop/src/main/java/DB/DBContext.java
@@ -4,10 +4,40 @@
  */
 package DB;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author KienBTCE180180
  */
 public class DBContext {
-    
+
+    protected Connection connector = null;
+
+    public DBContext() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // regist a class Database of Microsoft to memory
+
+            String dbURL = "jdbc:sqlserver://localhost:1433;"
+                    + "databaseName=FSHOP;"
+                    + "user=;" // Enter your user SQL Server
+                    + "password=;" // Enter your password SQL Server
+                    + "encrypt=true;trustServerCertificate=true";
+
+            connector = DriverManager.getConnection(dbURL); // connect to database server follow the dbURL string
+
+            if (connector == null) {
+                DatabaseMetaData dm = (DatabaseMetaData) connector.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
 }
