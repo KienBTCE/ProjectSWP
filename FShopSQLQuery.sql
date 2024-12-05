7b747483f3f4c2e49f7c91600bc02c66e96ed2ab
KienBTCE <KienBTCE180180@fpt.edu.vn>
2024-12-05 10:39:39 +0700

    create Accounts table in database and comment regulations for database

--- FShopSQLQuery.sql
+++ FShopSQLQuery.sql
@@ -18,3 +18,24 @@ GO
 
 USE FShop
 GO
+
+/*******************************************************************************
+   User camelCase for name of attribute. Ex: fullDateOfCreateAccount
+   Use prefix for attribute name of each table. Ex: Table Accounts have (a_id, a_fullName, a_email,...)
+********************************************************************************/
+
+CREATE TABLE Accounts (
+	a_role VARCHAR(10),
+	a_id VARCHAR(10),
+	a_fullName VARCHAR(100),
+	a_userName VARCHAR(100),
+	a_password VARCHAR(500),
+	a_email VARCHAR(254),
+	a_phoneNumber VARCHAR(15),
+	a_address VARCHAR(500),
+	a_birthday DATE,
+	a_gender CHAR(5) CHECK (a_Gender IN ('Mal', 'Female', 'Other')),
+	a_createTime DATETIME DEFAULT GETDATE(),
+	a_status VARCHAR(20) DEFAULT 'Active',
+	a_avatar TEXT
+)
