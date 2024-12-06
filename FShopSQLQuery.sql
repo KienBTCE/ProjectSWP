a4bc7c02b296da46fa5bd36ed45a8a2886ca0f9e
KienBTCE <KienBTCE180180@fpt.edu.vn>
2024-12-06 22:54:02 +0700

    change some key of Products table and some relationship

--- FShopSQLQuery.sql
+++ FShopSQLQuery.sql
@@ -21,103 +21,127 @@ GO
 
 /*******************************************************************************
    Use camelCase for name of attribute. Ex: fullDateOfCreateAccount
-   Use prefix for attribute name of each table. Ex: Table Accounts have (a_id, a_fullName, a_email,...)
+   Consider use prefix for attribute name of each table. Ex: Table Accounts have (a_id, a_fullName, a_email,...)
 ********************************************************************************/
 
 CREATE TABLE Accounts (
-	a_role VARCHAR(10) ,
-	a_ID VARCHAR(10) PRIMARY KEY,
+	a_ID VARCHAR(20) PRIMARY KEY, -- As a role
+
+	a_userName VARCHAR(100) NOT NULL,
+	a_password VARCHAR(500) NOT NULL,
+
 	a_fullName VARCHAR(255),
-	a_userName VARCHAR(100),
-	a_password VARCHAR(500),
-	a_email VARCHAR(254),
-	a_phoneNumber VARCHAR(15),
-	a_address VARCHAR(500),
-	a_birthday DATE,
-	a_gender CHAR(6) CHECK (a_Gender IN ('Male', 'Female', 'Other')),
-	a_createAt DATETIME DEFAULT GETDATE(),
-	a_status VARCHAR(20) DEFAULT 'Active',
-	a_avatar TEXT
+	birthday DATE,
+	gender CHAR(6) CHECK (gender IN ('Male', 'Female', 'Other')),
+
+	phoneNumber VARCHAR(15),
+	email VARCHAR(254),
+	createAt DATETIME DEFAULT GETDATE(),
+	[status] VARCHAR(20) DEFAULT 'Active',
+	avatar TEXT
 )
 
-CREATE TABLE Products (
-	pd_ID VARCHAR(20) PRIMARY KEY,
-	pd_fullName VARCHAR(255)
-)
-
-CREATE TABLE Phone (
-    p_ID VARCHAR(20) PRIMARY KEY,
-    p_fullName VARCHAR(255) NOT NULL,
-    p_screen VARCHAR(100),
-    p_camera VARCHAR(100),
-    p_RAM INT,
-    p_ROM INT,
-    p_size VARCHAR(50),
-    p_weight FLOAT,
-    p_price DECIMAL(10, 2) NOT NULL, 
-    p_image VARCHAR(255),
-    p_description TEXT,
-    p_brandId VARCHAR(20),
-    p_categoryId VARCHAR(20),
-	FOREIGN KEY (p_ID) REFERENCES Products(pd_ID)
-)
-
-CREATE TABLE Laptop (
-    lt_ID VARCHAR(20) PRIMARY KEY,
-    lt_fullName VARCHAR(255) NOT NULL,
-    lt_CPU VARCHAR(100),
-    lt_GPU VARCHAR(100),
-    lt_RAM INT,
-    lt_ROM INT,
-    lt_screen VARCHAR(100),
-    lt_camera VARCHAR(100),
-    lt_size VARCHAR(50),
-    lt_weight FLOAT,
-    lt_lightKeyboard BINARY,
-    lt_connectionPort TEXT,
-    lt_price DECIMAL(10, 2) NOT NULL,
-    lt_image VARCHAR(255),
-    lt_description TEXT,
-    lt_brandId VARCHAR(20),
-    lt_categoryId VARCHAR(20),
-	FOREIGN KEY (lt_ID) REFERENCES Products(pd_ID)
-)
-
-CREATE TABLE Categories (
-	ct_ID VARCHAR(20) PRIMARY KEY,
-	pd_ID VARCHAR(20),
-	ct_name VARCHAR(100),
-	FOREIGN KEY (pd_ID) REFERENCES Products(pd_ID)
-)
-
-CREATE TABLE Cart (
-	a_ID VARCHAR(10) ,
-	productId VARCHAR(20),
-	primary key (a_ID, productId),
-	quantity INT,
+CREATE TABLE Addresses (
+	a_ID VARCHAR(20),
+	[address] VARCHAR(500) UNIQUE,
 	FOREIGN KEY (a_ID) REFERENCES Accounts(a_ID)
 )
 
-CREATE TABLE [Order] (
+CREATE TABLE Products (
+	pd_ID VARCHAR(20),
+	brand VARCHAR(20),
+	pd_SKU INT UNIQUE IDENTITY(1,1),
+
+	PRIMARY KEY (pd_ID, brand),
+
+	[status] VARCHAR(20) CHECK ([status] IN ('Available', 'Comming Soon', 'Sold Out')),
+	note VARCHAR(500),
+	quantity INT NOT NULL
+)
+
+CREATE TABLE Phones (
+    pd_ID VARCHAR(20),
+	brand VARCHAR(20),
+
+    fullName VARCHAR(255) NOT NULL,
+    screen VARCHAR(100),
+    camera VARCHAR(100),
+    RAM INT,
+    ROM INT,
+	chip VARCHAR(100),
+    size VARCHAR(50),
+    [weight] FLOAT,
+
+    price DECIMAL(10, 2) NOT NULL, 
+
+    [image] VARCHAR(255),
+    [description] TEXT,
+    categoryId VARCHAR(20),
+	FOREIGN KEY (pd_ID, brand) REFERENCES Products(pd_ID, brand)
+)
+
+CREATE TABLE Laptops (
+    pd_ID VARCHAR(20),
+	brand VARCHAR(20),
+
+    fullName VARCHAR(255) NOT NULL,
+    screen VARCHAR(100),
+    camera VARCHAR(100),
+    RAM INT,
+    ROM INT,
+    CPU VARCHAR(100),
+    GPU VARCHAR(100),
+    size VARCHAR(50),
+    connectionPort TEXT,
+    lightKeyboard BINARY,
+    [weight] FLOAT,
+
+    price DECIMAL(10, 2) NOT NULL,
+
+    [image] VARCHAR(255),
+    [description] TEXT,
+    categoryId VARCHAR(20),
+	FOREIGN KEY (pd_ID, brand) REFERENCES Products(pd_ID, brand)
+)
+
+CREATE TABLE Categories (
+	ct_ID VARCHAR(20), -- this attribute will not change, follow to product to present type of product
+	pd_SKU INT,
+	PRIMARY KEY (ct_ID, pd_SKU),
+	typeName VARCHAR(100),
+	FOREIGN KEY (pd_SKU) REFERENCES Products(pd_SKU)
+)
+
+-- KIEN DA CHECK DEN CHO NAY HEHE :D MAI CHECK TIEP KEKE
+
+CREATE TABLE Carts (
+	a_ID VARCHAR(20),
+	productId VARCHAR(20),
+	PRIMARY KEY (a_ID, productId),
+	quantity INT NOT NULL,
+	FOREIGN KEY (a_ID) REFERENCES Accounts(a_ID)
+)
+
+CREATE TABLE [Orders] (
     orderID INT PRIMARY KEY IDENTITY(1,1),
-    a_ID VARCHAR(10) NOT NULL,
-    fullname VARCHAR(100) NOT NULL,
-    address TEXT NOT NULL,
-    productID INT, -- Consider removing if not required at the order level
+    a_ID VARCHAR(20) NOT NULL,
+    fullName VARCHAR(100) NOT NULL,
+    [address] TEXT NOT NULL,
+    productID VARCHAR(20), -- Consider removing if not required at the order level
     orderedDate DATE NOT NULL,
     orderShippedDate DATE,
     paymentMethod VARCHAR(20) NOT NULL,
-    status VARCHAR(20) NOT NULL,
+    [status] VARCHAR(20) NOT NULL,
     totalAmount DECIMAL(10, 2) NOT NULL,
     discount DECIMAL(10, 2),
     FOREIGN KEY (a_ID) REFERENCES Accounts(a_ID)
 )
 
-CREATE TABLE OrderDetail (
+CREATE TABLE OrderDetails (
     OrderID INT NOT NULL ,
-    ProductID INT NOT NULL,
+    ProductID VARCHAR(20) NOT NULL,
 	PRIMARY KEY (OrderID, ProductID),
     Quantity INT NOT NULL,
     Price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (OrderID) REFERENCES [Order](OrderID)
+    FOREIGN KEY (OrderID) REFERENCES [Orders](OrderID)
 )
