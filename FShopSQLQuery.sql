82edec1fc9eea66f510d51aba6fffd9ab30170ac
nhutbmce180569 <nhutbmce180569@fpt.edu.vn>
2025-01-10 16:29:25 +0700

    Add some attribute is lack

--- FShopSQLQuery.sql
+++ FShopSQLQuery.sql
@@ -24,9 +24,9 @@ GO
    Consider use prefix for attribute name of each table. Ex: Table Accounts have (a_id, a_fullName, a_email,...)
 ********************************************************************************/
 
-CREATE TABLE [Role] (
+CREATE TABLE [Roles] (
 	[roleID] INT Primary key,
-	[name] NVARCHAR(50)
+	[name] NVARCHAR(50) NOT NULL
 )
 
 CREATE TABLE Accounts (
@@ -35,14 +35,14 @@ CREATE TABLE Accounts (
 	email VARCHAR(254),
 	[password] VARCHAR(500) NOT NULL,
 	fullName VARCHAR(255) NOT NULL,
-	birthday DATE,
+	birthday DATE NOT NULL,
 	gender CHAR(6) CHECK (gender IN ('Male', 'Female', 'Other')),
 	createAt DATETIME DEFAULT GETDATE(),
 	[status] VARCHAR(20) DEFAULT 'Active',
 	avatar TEXT,
-	roleID INT,
-	FOREIGN KEY (roleID) REFERENCES [Role](roleID)
-
+	roleID INT NOT NULL,
+	loyalPoint BIGINT,
+	FOREIGN KEY (roleID) REFERENCES [Roles](roleID)
 )
 
 
@@ -55,16 +55,16 @@ WHERE email IS NOT NULL;
 
 CREATE TABLE Addresses (
 	a_ID INT,
-	street NTEXT,
-	ward NVARCHAR(100),
-	district NVARCHAR(100),
-	city NVARCHAR(100),
+	street NTEXT NOT NULL,
+	ward NVARCHAR(100) NOT NULL,
+	district NVARCHAR(100) NOT NULL,
+	city NVARCHAR(100) NOT NULL,
 	FOREIGN KEY (a_ID) REFERENCES Accounts(a_ID)
 )
 
 CREATE TABLE Categories (
-	ct_ID INT IDENTITY(1,1) primary key, -- this attribute will not change, follow to product to present type of product
-	typeName VARCHAR(100),
+	ct_ID INT primary key, -- this attribute will not change, follow to product to present type of product
+	typeName VARCHAR(100) NOT NULL,
 )
 
 CREATE TABLE Products (
@@ -76,9 +76,9 @@ CREATE TABLE Products (
 	[status] VARCHAR(20) CHECK ([status] IN ('Available', 'Comming Soon', 'Sold Out')) DEFAULT 'Available',
 	note VARCHAR(500),
 	quantity INT NOT NULL,
-	[productType] VARCHAR(100) NOT NULL,
 	price INT NOT NULL,
-	categoryID INT,
+	importDate DATE,
+	categoryID INT NOT NULL,
 	FOREIGN KEY (categoryID) REFERENCES Categories(ct_ID)
 )
 
@@ -118,7 +118,6 @@ CREATE TABLE Laptops (
 CREATE TABLE Carts (
 	a_ID INT,
 	pd_SKU INT,
-	PRIMARY KEY (a_ID, pd_SKU),
 	quantity INT NOT NULL,
 	FOREIGN KEY (a_ID) REFERENCES Accounts(a_ID),
 	FOREIGN KEY (pd_SKU) REFERENCES Products(pd_SKU)
@@ -141,10 +140,10 @@ CREATE TABLE [Orders] (
 CREATE TABLE OrderDetails (
     orderID INT NOT NULL ,
     pd_SKU INT NOT NULL,
-	PRIMARY KEY (OrderID, pd_SKU),
+	PRIMARY KEY (orderID, pd_SKU),
     quantity INT NOT NULL,
     price INT NOT NULL,
-    FOREIGN KEY (OrderID) REFERENCES [Orders](OrderID)
+    FOREIGN KEY (orderID) REFERENCES [Orders](orderID)
 )
 
 CREATE TABLE Blogs (
@@ -152,7 +151,8 @@ CREATE TABLE Blogs (
 	writer INT,
 	blogTitle NTEXT,
 	content NTEXT,
-	images TEXT
+	images TEXT,
+		datePost DATE DEFAULT GETDATE(),
 	Foreign key (writer) References Accounts(a_ID)
 )
 /*******************************************************************************
@@ -161,32 +161,34 @@ CREATE TABLE Blogs (
 
 SET IDENTITY_INSERT Products ON;
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (1, 'Asus', '14XHMY', 'Zenbook 14XHMY', 10, 'Available', 'Laptop', 28909000)
+INSERT INTO Categories VALUES (1, 'Laptop'), (2, 'Smartphone')
+
+
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]   , price, categoryID) VALUES (1, 'Asus', '14XHMY', 'Zenbook 14XHMY', 10, 'Available', 28909000, 1)
 INSERT INTO Laptops(pd_SKU, [image]) VALUES (1, '250-8562-line-laptop.png')
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (2, 'Apple', '12XHMY', 'iPhone 15 Pro Max 256GB', 10, 'Available', 'SmartPhone', 32529000)
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]   , price, categoryID) VALUES (2, 'Apple', '12XHMY', 'iPhone 15 Pro Max 256GB', 10, 'Available', 32529000, 2)
 INSERT INTO Phones(pd_SKU, [image]) VALUES (2, 'iphone-16-pro-max-black-thumb-600x600.jpg')
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (3, 'Samsung', '7XGLX', 'Galaxy S23 512GB', 10, 'Available', 'SmartPhone', 25899000)
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]  , price , categoryID) VALUES (3, 'Samsung', '7XGLX', 'Galaxy S23 512GB', 10, 'Available',  25899000, 2)
 INSERT INTO Phones(pd_SKU, [image]) VALUES (3, 'samsung-galaxy-s23-xanh-600x600-1.jpg')
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (4, 'Lenovo', '15LGXHYM', 'Legion Pro 15GXH', 10, 'Available', 'Laptop', 38909000)
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]  , price , categoryID) VALUES (4, 'Lenovo', '15LGXHYM', 'Legion Pro 15GXH', 10, 'Available',  38909000, 1)
 INSERT INTO Laptops(pd_SKU, [image]) VALUES (4, '0yp3jx9d-1090-lenovo-legion-pro-5-y9000p-2023-core-i9-13900hx-16gb-1tb-rtx-4050-6gb-16-wqxga-240hz-new.jpg')
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (5, 'Asus', 'A17FA706', 'Asus Tuf Gamming 17FA', 10, 'Available', 'Laptop', 31909000)
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]   , price , categoryID) VALUES (5, 'Asus', 'A17FA706', 'Asus Tuf Gamming 17FA', 10, 'Available', 31909000, 1)
 INSERT INTO Laptops(pd_SKU, [image]) VALUES (5, 'ASUS-TUF-Gaming-A17-FA706-600x600.jpg')
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (6, 'MSI', 'b8vf406ca', 'MSI Katana Gamming B8VF', 10, 'Available', 'Laptop', 33909000)
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]   , price , categoryID) VALUES (6, 'MSI', 'b8vf406ca', 'MSI Katana Gamming B8VF', 10, 'Available',  33909000, 1)
 INSERT INTO Laptops(pd_SKU, [image]) VALUES (6, '5e0dkkrb-1411-msi-katana-gaming-a15-ai-b8vf-406ca-amd-ryzen-r9-8945hs-32gb-1tb-rtx-4060-8gb-15-6-144hz-fhd-new.jpg')
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (7, 'Apple', 'MCBP2021', 'Macbook Pro 2021', 10, 'Available', 'Laptop', 45909000)
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]  , price , categoryID) VALUES (7, 'Apple', 'MCBP2021', 'Macbook Pro 2021', 10, 'Available', 45909000, 1)
 INSERT INTO Laptops(pd_SKU, [image]) VALUES (7, '250-7038-macbook-pro-2021-apple-m1-1.jpg')
 
-INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (8, 'Dell', 'D9C72G1', 'Dell Gamming G15 551', 10, 'Available', 'Laptop', 35909000)
+INSERT INTO Products(pd_SKU, brand, pd_ID, fullName, quantity, [status]   , price , categoryID) VALUES (8, 'Dell', 'D9C72G1', 'Dell Gamming G15 551', 10, 'Available', 35909000, 1)
 INSERT INTO Laptops(pd_SKU, [image]) VALUES (8, '45606_dell_gaming_5511_dark_grey_ha3.jpg')
 
-SET IDENTITY_INSERT Products OFF;
-INSERT [Role] VALUES (1, 'Admin'), (2, 'Manager'), (3, 'Staff'), (4, 'Customer')
+INSERT [Roles] VALUES (1, 'Admin'), (2, 'Manager'), (3, 'Staff'), (4, 'Customer')
 
 INSERT INTO Accounts(phoneNumber, email, [password], fullName, birthday, gender, avatar, roleID)
 VALUES ('0888888888', 'abc123@gmail.com', 'user1', 'User NoName', '2004-01-01', 'Male', 'avatar', 4)
@@ -194,10 +196,13 @@ VALUES ('0888888888', 'abc123@gmail.com', 'user1', 'User NoName', '2004-01-01', 
 INSERT INTO Carts VALUES (1, 1, 1),
 (1, 2, 1),
 (1, 5, 1),
+(1, 7, 5),
 (1, 7, 5)
 
 Insert into [Orders] (a_ID, fullName, a_phoneNumber, [address], paymentMethod, totalAmount) values 
 (1, 'Nguyen Van a', '034xxxxxxx',N'Địa chỉ có dấu', 'Delivery', 100000000)
+
+SET IDENTITY_INSERT Products OFF;
 
 Select * from Orders
 order by orderID desc
@@ -207,6 +212,8 @@ Select * from OrderDetails
 Select * from Carts
 
 Select * from Accounts
+
+Select * from Products
 --Delete from Carts where pd_SKU = 2 and a_ID LIKE 'user1'
 
 SELECT * FROM Laptops JOIN Products
