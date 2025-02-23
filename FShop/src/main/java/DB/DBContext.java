75e0cf4814b78e8bf33c402482d1c276ea5bc799
KienBTCE <KienBTCE180180@fpt.edu.vn>
2025-02-23 14:48:17 +0700

    fix product

--- FShop/src/main/java/DB/DBContext.java
+++ FShop/src/main/java/DB/DBContext.java
@@ -24,7 +24,7 @@ public class DBContext {
             String dbURL = "jdbc:sqlserver://localhost:1433;"
                     + "databaseName=FSHOP;"
                     + "user=sa;" // Enter your user SQL Server
-                    + "password=admin;" // Enter your password SQL Server
+                    + "password=12345;" // Enter your password SQL Server
                     + "encrypt=true;trustServerCertificate=true";
 
             connector = DriverManager.getConnection(dbURL); // connect to database server follow the dbURL string
