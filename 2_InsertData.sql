9491567e7d652685b8b7a7f44adf1c1912a0c516
KienBTCE <KienBTCE180180@fpt.edu.vn>
2025-02-24 14:42:25 +0700

    add more products

--- 2_InsertData.sql
+++ 2_InsertData.sql
@@ -38,35 +38,27 @@ VALUES
 -- Insert Products
 SET IDENTITY_INSERT Products ON;
 
-INSERT INTO Products (BrandID, CategoryID, Model, FullName, [Description], [Image], Price)
+INSERT INTO Products (ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], Price)
 VALUES 
-(1, 1, 'Macbook Pro 14', 'Apple Macbook Pro 14 inch M1 Pro chip', 'New model from Apple', '250-7038-macbook-pro-2021-apple-m1-1.jpg', 50000000),
-(2, 2, 'Galaxy S23 Ultra', 'Samsung Galaxy S23 Ultra 5G 256GB', 'Latest flagship from Samsung', 'samsung-galaxy-s23-xanh-600x600-1.jpg', 30000000),
-(3, 1, 'Sony Vaio Z900', 'Sony Vaio Z900 Core i7 16GB RAM', 'High-performance laptop', 'vaio-z900.jpg', 20000000);
+(1, 1, 1, 'Macbook Pro 14', 'Apple Macbook Pro 14 inch M1 Pro chip', 'New model from Apple', '250-7038-macbook-pro-2021-apple-m1-1.jpg', 50000000),
+(2, 2, 2, 'Galaxy S23 Ultra', 'Samsung Galaxy S23 Ultra 5G 256GB', 'Latest flagship from Samsung', 'samsung-galaxy-s23-xanh-600x600-1.jpg', 30000000),
+(3, 3, 1, 'Sony Vaio Z900', 'Sony Vaio Z900 Core i7 16GB RAM', 'High-performance laptop', 'vaio-z900.jpg', 20000000);
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (1, 'Asus', '14XHMY', 'Zenbook 14XHMY', 10, 'Available', 'Laptop', 28909000)
-INSERT INTO Laptops(ProductID, [image]) VALUES (1, '250-8562-line-laptop.png')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (4, 8, 1, '', '', '', '250-8562-line-laptop.png', 28909000)
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (2, 'Apple', '12XHMY', 'iPhone 15 Pro Max 256GB', 10, 'Available', 'SmartPhone', 32529000)
-INSERT INTO Phones(ProductID, [image]) VALUES (2, 'iphone-16-pro-max-black-thumb-600x600.jpg')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (5, 1, 2, '', '', '', 'iphone-16-pro-max-black-thumb-600x600.jpg', 32529000)
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (3, 'Samsung', '7XGLX', 'Galaxy S23 512GB', 10, 'Available', 'SmartPhone', 25899000)
-INSERT INTO Phones(ProductID, [image]) VALUES (3, 'samsung-galaxy-s23-xanh-600x600-1.jpg')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (6, 2, 2, '', '', '', 'samsung-galaxy-s23-xanh-600x600-1.jpg', 25899000)
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (4, 'Lenovo', '15LGXHYM', 'Legion Pro 15GXH', 10, 'Available', 'Laptop', 38909000)
-INSERT INTO Laptops(ProductID, [image]) VALUES (4, '0yp3jx9d-1090-lenovo-legion-pro-5-y9000p-2023-core-i9-13900hx-16gb-1tb-rtx-4050-6gb-16-wqxga-240hz-new.jpg')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (7, 9, 1, '', '', '', '0yp3jx9d-1090-lenovo-legion-pro-5-y9000p-2023-core-i9-13900hx-16gb-1tb-rtx-4050-6gb-16-wqxga-240hz-new.jpg', 38909000)
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (5, 'Asus', 'A17FA706', 'Asus Tuf Gamming 17FA', 10, 'Available', 'Laptop', 31909000)
-INSERT INTO Laptops(ProductID, [image]) VALUES (5, 'ASUS-TUF-Gaming-A17-FA706-600x600.jpg')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (8, 8, 1, '', '', '', 'ASUS-TUF-Gaming-A17-FA706-600x600.jpg', 31909000)
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (6, 'MSI', 'b8vf406ca', 'MSI Katana Gamming B8VF', 10, 'Available', 'Laptop', 33909000)
-INSERT INTO Laptops(ProductID, [image]) VALUES (6, '5e0dkkrb-1411-msi-katana-gaming-a15-ai-b8vf-406ca-amd-ryzen-r9-8945hs-32gb-1tb-rtx-4060-8gb-15-6-144hz-fhd-new.jpg')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (9, 11, 1, '', '', '', '5e0dkkrb-1411-msi-katana-gaming-a15-ai-b8vf-406ca-amd-ryzen-r9-8945hs-32gb-1tb-rtx-4060-8gb-15-6-144hz-fhd-new.jpg', 33909000)
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (7, 'Apple', 'MCBP2021', 'Macbook Pro 2021', 10, 'Available', 'Laptop', 45909000)
-INSERT INTO Laptops(ProductID, [image]) VALUES (7, '250-7038-macbook-pro-2021-apple-m1-1.jpg')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (10, 1, 1, '', '', '', '250-7038-macbook-pro-2021-apple-m1-1.jpg', 45909000)
 
-INSERT INTO Products(ProductID, brand, pd_ID, fullName, quantity, [status], [productType], price) VALUES (8, 'Dell', 'D9C72G1', 'Dell Gamming G15 551', 10, 'Available', 'Laptop', 35909000)
-INSERT INTO Laptops(ProductID, [image]) VALUES (8, '45606_dell_gaming_5511_dark_grey_ha3.jpg')
+INSERT INTO Products(ProductID, BrandID, CategoryID, Model, FullName, [Description], [Image], price) VALUES (11, 6, 1, '', '', '', '45606_dell_gaming_5511_dark_grey_ha3.jpg', 35909000)
 
 SET IDENTITY_INSERT Products OFF;
 
