39e99270e446fe47d25b577b429f528e1863f728
TuongMPCE180644 <TuongMPCE180644@fpt.edu.vn>
2025-03-03 07:24:33 +0700

    change some bug

--- FShop/src/main/java/Controllers/EmployeeLoginServlet.java
+++ FShop/src/main/java/Controllers/EmployeeLoginServlet.java
@@ -93,7 +93,7 @@ public class EmployeeLoginServlet extends HttpServlet {
                     response.sendRedirect("/ViewOrderListServlet");//Link qua Order Manager
                 } else if(em.getRoleId() == 4){
                     session.setAttribute("message", "Warehouse Manager");
-                    response.sendRedirect("/EmployeeLogin");//Link qua Warehouse Manager
+                    response.sendRedirect("/Supplier");//Link qua Warehouse Manager
                 } else{
                     processRequest(request, response);//ko có roleId thì error
                 }
