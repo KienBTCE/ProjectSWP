6b8fff5c06656af6fecf85c53afe239e7107afbd
nhutbmce180569 <nhutbmce180569@fpt.edu.vn>
2025-03-27 22:08:46 +0700

    update UI

--- FShop/src/main/java/DB/DBContext.java
+++ FShop/src/main/java/DB/DBContext.java
@@ -21,7 +21,7 @@ public class DBContext {
             String dbURL = "jdbc:sqlserver://localhost:1433;"
                     + "databaseName=FSHOP;"
                     + "user=sa;" 
-                    + "password=123;"
+                    + "password=admin;"
                     + "encrypt=true;trustServerCertificate=true";
 
             connector = DriverManager.getConnection(dbURL);
