dd4ffbec5aa979070c567192f1334b785ead5a7f
ThNguien <nguyenpvtce181835@fpt.edu.vn>
2025-03-28 02:02:54 +0700

    them chuot

--- FShop/src/main/java/Controllers/CreateVoucherServlet.java
+++ FShop/src/main/java/Controllers/CreateVoucherServlet.java
@@ -15,10 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-/**
- *
- * @author HP
- */
+
 public class CreateVoucherServlet extends HttpServlet {
 
     /**
@@ -62,14 +59,7 @@ public class CreateVoucherServlet extends HttpServlet {
 
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -140,14 +130,5 @@ public class CreateVoucherServlet extends HttpServlet {
         }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
